37bb667c446ea0a325b240f62c81e469d15aa6b9
kehwa weng <kehwaweng@ipevodeMacBook-Pro-2.local>
2023-06-13 15:02:38 +0800

    add fake conn

--- server/fake_conn.go
+++ server/fake_conn.go
@@ -1,50 +1,13 @@
-
 package server
 
 import (
-    "io"
+	"io"
 )
 
 type FakeConn struct {
-    io.ReadWriter
+	io.ReadWriter
 }
 
 func (c *FakeConn) Close() error {
-    return nil
-}
-
-----------
-
-protocol/protocol.go
-
-func (p *Protocol) PUB(client StatefulReadWriter, params []string) ([]byte, error) {
-    var buf bytes.Buffer
-    var err error
-
-    //  fake clients don't get to ClientInit
-    if client.GetState() != -1 {
-        return nil, ClientErrInvalid
-    }
-
-    if len(params) < 3 {
-        return nil, ClientErrInvalid
-    }
-
-    topicName := params[1]
-    body := []byte(params[2])
-
-    _, err = buf.Write(<-util.UuidChan)
-    if err != nil {
-        return nil, err
-    }
-
-    _, err = buf.Write(body)
-    if err != nil {
-        return nil, err
-    }
-
-    topic := message.GetTopic(topicName)
-    topic.PutMessage(message.NewMessage(buf.Bytes()))
-
-    return []byte("OK"), nil
+	return nil
 }
